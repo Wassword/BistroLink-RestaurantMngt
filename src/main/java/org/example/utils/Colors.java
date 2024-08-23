b956328be21fb0d7cf33e3744832950994af6add
BDNIKKO <moppertn@gmail.com>
2024-08-23 14:51:01 -0400

    done

--- src/main/java/org/example/utils/Colors.java
+++ src/main/java/org/example/utils/Colors.java
@@ -4,7 +4,6 @@ package org.example.utils;
 
 public class Colors {
     public static final String RESET = "\u001B[0m";
-    public static final String BLUE = "\u001B[34m";
     public static final String RED = "\u001B[31m";
     public static final String GREEN = "\u001B[32m";
     public static final String YELLOW = "\u001B[33m";
